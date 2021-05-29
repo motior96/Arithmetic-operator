a2658ade71da7d2740a174c8f74b6a3c6f24aa7e
motior96 <you@examle.com>
2021-05-29 20:02:24 +0600

    file added

---
+++ js/cutom.js
@@ -0,0 +1,71 @@
+function add(){
+    var x,y;
+    x = 5;
+    y = 6;
+    z = x + y;
+    document.getElementById('one').innerHTML = z;
+}
+
+function sub(){
+    var x,y;
+    x = 6;
+    y = 5;
+    z = x - y;
+    document.getElementById('two').innerHTML = z;
+}
+
+function multiplies(){
+    var x,y;
+    x = 6;
+    y = 5;
+    z = x * y;
+    document.getElementById('three').innerHTML = z;
+}
+
+function division(){
+    var x,y;
+    x = 6;
+    y = 5;
+    z = x / y;
+    document.getElementById('four').innerHTML = z;
+}
+
+function exponent(){
+    var x = 6;
+    var y = 3;
+    document.getElementById('five').innerHTML = x ** y ;
+}
+
+function modul(){
+    var x,y;
+    x = 6;
+    y = 3;
+    document.getElementById('six').innerHTML = x % y;
+}
+
+function incre(){
+    var x = 6;
+    x ++;
+    document.getElementById('seven').innerHTML = x;
+}
+
+function decre(){
+    var x = 6;
+    x --;
+    document.getElementById('eight').innerHTML = x;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
